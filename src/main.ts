e7c4bb254a708c09a6ec5e205f08b7c29bc6518a
dmitriy <deemaphp@outlook.com>
2021-08-21 17:17:01 +0300

    add basic swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Item API')
+      .setDescription('My Item API')
+      .build(),
+  );
+
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
