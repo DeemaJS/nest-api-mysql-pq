e45eb43fa83c3a64f6055b00ad3cb96b8754d974
dmitriy <deemaphp@outlook.com>
2021-08-21 13:49:56 +0300

    Basic init project

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy],
+  imports: [UsersModule, PassportModule],
 })
 export class AuthModule {}
